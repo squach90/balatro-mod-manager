86b2f96dbca3a7ac8a30070562655373c17462c1
Ö. Efe D. <67526259+skyline69@users.noreply.github.com>
2025-02-27 23:48:14 +0100

    Update to v0.1.6

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -98,7 +98,7 @@
 <div class="app">
 	<h1>Welcome to Balatro Mod Manager</h1>
 	<BalatroPicker />
-	<div class="version-text">v0.1.5</div>
+	<div class="version-text">v0.1.6</div>
 </div>
 
 <style>
--- src/routes/main/+page.svelte
+++ src/routes/main/+page.svelte
@@ -184,7 +184,7 @@
 		{onError}
 	/>
 
-	<div class="version-text">v0.1.5</div>
+	<div class="version-text">v0.1.6</div>
 </div>
 
 <style>
