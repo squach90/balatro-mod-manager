aea955481618774e2a0074a31470bc7c11a64112
Ö. Efe D. <67526259+skyline69@users.noreply.github.com>
2025-03-11 21:59:04 +0100

    Fix Background for Windows

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,16 +2,25 @@
 	import { blur } from "svelte/transition";
 	import MessageStack from "../components/MessageStack.svelte";
 	import { backgroundEnabled } from "../stores/modStore";
+	import { onMount } from "svelte";
 
 	import "../app.css";
 
 	export let data;
+
+	let isWindows = false;
+
+	onMount(() => {
+		isWindows = navigator.userAgent.indexOf("Windows") !== -1;
+	});
 </script>
 
 <MessageStack />
 <div
 	class="layout-container"
 	style:--gradient-opacity={$backgroundEnabled ? 0 : 1}
+	style:--dot-size={isWindows ? "1.5px" : "0.45px"}
+	style:--dot-color={isWindows ? "#ff9999" : "#d66060"}
 >
 	{#key data.url}
 		<div
@@ -44,8 +53,14 @@
 		opacity: var(--gradient-opacity, 1);
 		transition: opacity 0.4s cubic-bezier(0.4, 0, 0.2, 1);
 		background-color: #a53535;
-		background-image: radial-gradient(#d66060 0.45px, transparent 0.45px),
-			radial-gradient(#d66060 0.45px, #a53535 0.45px);
+		background-image: radial-gradient(
+				var(--dot-color, #d66060) var(--dot-size, 0.45px),
+				transparent var(--dot-size, 0.45px)
+			),
+			radial-gradient(
+				var(--dot-color, #d66060) var(--dot-size, 0.45px),
+				#a53535 var(--dot-size, 0.45px)
+			);
 		background-size: 18px 18px;
 		background-position:
 			0 0,
@@ -58,6 +73,15 @@
 		height: 100%;
 		position: relative;
 		overflow: hidden;
+	}
+
+	@media screen and (min-width: 1920px) {
+		.layout-container::before {
+			background-size: 24px 24px;
+			background-position:
+				0 0,
+				12px 12px;
+		}
 	}
 </style>
 
